7bba4f36a768424d21ab9a1e7db21e5ab1c68e2c
iakmds <111225446+iakmds@users.noreply.github.com>
2023-04-17 21:46:11 +0200

    update: sub_menu.dart

--- lib/src/sub_menu.dart
+++ lib/src/sub_menu.dart
@@ -1,16 +1,10 @@
 import 'entry.dart';
-import 'menu.dart';
 
-class SubMenu extends MenuBarEntryMenu {
+class SubMenu {
   /// A [SubMenu] is displayed as a submenu with the [menuItems] field as its items.
   ///
-  /// The following field is necessary: [menuItems].
-  ///
   /// Assign a List of either [MenuButton] or [MenuDivider] widgets to the [menuItems] field. The List should contain at least one item.
-  ///
-  /// You can style the [SubMenu] widgets in the `menuStyle` field of your MenuBarWidget.
-  const SubMenu({
-    super.key,
-    required List<MenuEntry> menuItems,
-  }) : super(menuItems: menuItems, text: null, submenu: null);
+  const SubMenu({required this.menuItems}) : super();
+
+  final List<MenuEntry> menuItems;
 }
